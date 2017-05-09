8879797ca4bad2be7ac7f484e52b6a92b9cdab5e
lucas danilo longo <llongo@ws9.labo5>
2017-05-09 16:36:04 -0300

    "Ising llena  e imprime red"

--- src/ising.c
+++ src/ising.c
@@ -1,8 +1,8 @@
-#include "stdlib.h"
-#include "time.h"
-
+#include <stdlib.h>
+#include <time.h>
 #include "metropolis.h"
 #include "lattice.h"
+#include <unistd.h>
 
 int main(int argc, char **argv) {
   int n = 32;
@@ -12,9 +12,9 @@ int main(int argc, char **argv) {
   int niter = 2000;
   srand(time(NULL));
   fill_lattice(lattice, n, prob);
-  for (int i = 0; i < niter; i++) {
-    metropolis(lattice, n, T);
-  }
+  // for (int i = 0; i < niter; i++) {
+  // metropolis(lattice, n, T);
+  // }
   print_lattice(lattice, n);
   return 0;
 }
