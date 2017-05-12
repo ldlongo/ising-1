0e6c8d8fa57a2b4949df5e2a6e37d01b82ad42b7
lucas danilo longo <llongo@ws8.labo5>
2017-05-12 16:10:25 -0300

    "Clase 12-05"

--- src/ising.c
+++ src/ising.c
@@ -5,9 +5,9 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-  int n = 32;
+  int n = 16;
   int *lattice = malloc(n * n * sizeof(int));
-  float prob = 0.5;
+  float prob = 0.1;
   float T = 2.0;
   int niter = 2000;
   srand(time(NULL));
@@ -15,6 +15,7 @@ int main(int argc, char **argv) {
   // for (int i = 0; i < niter; i++) {
   // metropolis(lattice, n, T);
   // }
+  pick_site(lattice, n);
   print_lattice(lattice, n);
   return 0;
 }
