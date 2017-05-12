2b713b87b9cfc5892551bd2cc39d0766fea04468
lucas danilo longo <llongo@ws8.labo5>
2017-05-12 16:11:04 -0300

    "Numero aleatorio entre 1 y n*n"

--- src/metropolis.c
+++ src/metropolis.c
@@ -1,13 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "lattice.h"
 #include "metropolis.h"
 
-int metropolis(int *lattice, int n, float T) {
-  return 0;
-}
+///int metropolis(int *lattice, int n, float T) {
+// return 0;
+//}
 
 int pick_site(int *lattice, int n) {
-  return 0;
+  int j;
+  int idx; //idx es la posicion del vector lattice que voy a  flipear( 1< idx< n*n)
+  idx =(float)rand()*n*n/(float)RAND_MAX;
+  printf("%d\n",idx);
+  return  ;
 }
 
-int flip(int *lattice, int n, float T) {
-  return 0;
+int flip(int *lattice, int n, float T, float idx) {
+  *lattice[idx]=*lattice[idx]*(-1);
+return 0;
 }
