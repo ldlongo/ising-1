deded381dc0f42d8a2c9c2190289d028d0c7e1c3
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-05-12 19:15:56 -0300

    "Funcionando el pick y el flip"

--- src/ising.c
+++ src/ising.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 #include "metropolis.h"
 #include "lattice.h"
-#include <unistd.h>
 
 int main(int argc, char **argv) {
-  int n = 16;
+  int n = 4;
   int *lattice = malloc(n * n * sizeof(int));
   float prob = 0.1;
   float T = 2.0;
   int niter = 2000;
+  int idx;
   srand(time(NULL));
   fill_lattice(lattice, n, prob);
   // for (int i = 0; i < niter; i++) {
-  // metropolis(lattice, n, T);
-  // }
-  pick_site(lattice, n);
+   metropolis(lattice, n, T);
+  // } 
   print_lattice(lattice, n);
   return 0;
 }
