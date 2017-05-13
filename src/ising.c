f38ed36a8a40edb2b37fa8675b512c65c53a14f9
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-05-13 00:21:55 -0300

    "Imprimo red y su energia"

--- src/ising.c
+++ src/ising.c
@@ -6,7 +6,7 @@
 #include "lattice.h"
 
 int main(int argc, char **argv) {
-  int n = 4;
+  int n = 3;
   int *lattice = malloc(n * n * sizeof(int));
   float prob = 0.1;
   float T = 2.0;
@@ -14,9 +14,9 @@ int main(int argc, char **argv) {
   int idx;
   srand(time(NULL));
   fill_lattice(lattice, n, prob);
-  // for (int i = 0; i < niter; i++) {
-   metropolis(lattice, n, T);
-  // } 
   print_lattice(lattice, n);
+  // for (int i = 0; i < niter; i++) {
+  metropolis(lattice, n, T);
+  // } 
   return 0;
 }
