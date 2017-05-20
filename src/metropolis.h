b675a9624d787bba6c5663f5095e287b63b363a9
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-05-20 14:28:47 -0300

    "Agrego float J, y función CPC"

--- src/metropolis.h
+++ src/metropolis.h
@@ -1,9 +1,10 @@
 #ifndef METROPOLIS_H
 #define METROPOLIS_H
-int metropolis(int *lattice, int n, float T, float H, float *pE, int *pM);
+int metropolis(int *lattice, int n, float T, float H, float J, float *pE, int *pM);
 int pick_site(int *lattice, int n);
-int flip(int *lattice, int n, float T, int idx, float H, float *pE, int *pM);
+int flip(int *lattice, int n, float T, int idx, float H, float J, float *pE, int *pM);
 int imprimir(int *lattice, int n);
-int energia(int *lattice, int n, float H);
+int energia(int *lattice, int n, float H, float J);
 int magnetizacion(int *lattice,int n);
+int CPC(int *lattice, int n, int i, int j, int* spinborde);
 #endif
