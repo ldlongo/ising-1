f449e46cdb077728d22820c1bff848deb93f5427
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-05-20 03:30:24 -0300

    "Cambio la aridad agrego pE y pM a la funcion metropolis"

--- src/metropolis.h
+++ src/metropolis.h
@@ -1,9 +1,9 @@
 #ifndef METROPOLIS_H
 #define METROPOLIS_H
-int metropolis(int *lattice, int n, float T);
+int metropolis(int *lattice, int n, float T, float H, float *pE, int *pM);
 int pick_site(int *lattice, int n);
-int flip(int *lattice, int n, float T, int idx);
+int flip(int *lattice, int n, float T, int idx, float H, float *pE, int *pM);
 int imprimir(int *lattice, int n);
-int energia(int *lattice, int n);
+int energia(int *lattice, int n, float H);
 int magnetizacion(int *lattice,int n);
 #endif
