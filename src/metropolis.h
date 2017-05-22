2910e8e997c4519a40235fc98854422dd21f26ad
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-05-22 19:28:37 -0300

    "Agrego en la aridad de metropolis y de flip file *f"

--- src/metropolis.h
+++ src/metropolis.h
@@ -1,8 +1,8 @@
 #ifndef METROPOLIS_H
 #define METROPOLIS_H
-int metropolis(int *lattice, int n, float T, float H, float J, float *pE, int *pM);
+int metropolis(int *lattice, int n, float T, float H, float J, float *pE, int *pM, FILE *f);
 int pick_site(int *lattice, int n);
-int flip(int *lattice, int n, float T, int idx, float H, float J, float *pE, int *pM);
+int flip(int *lattice, int n, float T, int idx, float H, float J, float *pE, int *pM, FILE *f);
 int imprimir(int *lattice, int n);
 int energia(int *lattice, int n, float H, float J);
 int magnetizacion(int *lattice,int n);
