44024df08035c1f385197e8f9bd6a3479e9513f6
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-05-23 02:00:48 -0300

    "Funcion de correlacion"

--- src/metropolis.h
+++ src/metropolis.h
@@ -1,10 +1,12 @@
 #ifndef METROPOLIS_H
 #define METROPOLIS_H
-int metropolis(int *lattice, int n, float T, float H, float J, float *pE, int *pM, FILE *f);
+int metropolis(int *lattice, int n, float T, float H, float J, float *pE, int *pM, FILE *f, float *ecorr, int *contador);
 int pick_site(int *lattice, int n);
-int flip(int *lattice, int n, float T, int idx, float H, float J, float *pE, int *pM, FILE *f);
+int flip(int *lattice, int n, float T, int idx, float H, float J, float *pE, int *pM, FILE *f, float *ecorr, int *contador);
 int imprimir(int *lattice, int n);
 int energia(int *lattice, int n, float H, float J);
 int magnetizacion(int *lattice,int n);
 int CPC(int *lattice, int n, int i, int j, int* spinborde);
+int correlacion(float *ecorr, int *contador,FILE *g);
 #endif
+
