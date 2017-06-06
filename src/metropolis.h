b569f35e6eb4ec134d5cd88512ff4639d6ad4500
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-06-05 23:03:17 -0300

    "Cambio aridad, agrego K como interaccion antiferro"

--- src/metropolis.h
+++ src/metropolis.h
@@ -1,10 +1,10 @@
 #ifndef METROPOLIS_H
 #define METROPOLIS_H
-int metropolis(int *lattice, int n, float T, float H, float J, float *pE, int *pM);
+int metropolis(int *lattice, int n, float T, float H, float J, float K,float *pE, int *pM);
 int pick_site(int *lattice, int n);
-int flip(int *lattice, int n, float T, int idx, float H, float J, float *pE, int *pM);
+int flip(int *lattice, int n, float T, int idx, float H, float J, float K,float *pE, int *pM);
 int imprimir(int *lattice, int n);
-int energia(int *lattice, int n, float H, float J);
+int energia(int *lattice, int n, float H, float J, float K);
 int magnetizacion(int *lattice,int n);
 int CPC(int *lattice, int n, int i, int j, int* spinborde);
 int correlacion(float *e, int contador, int n,float T, float H, float J);
