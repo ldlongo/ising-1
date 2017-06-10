71bfc4a1b75f9ea6ba316b1384213ab21223ab2b
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-06-10 16:46:03 -0300

    "Cv vs J"

---
+++ bin/b) corracople/cv/CvsT.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+calor={} #calor
+
+#tamano
+n=32
+
+acople="acople.txt"
+
+#extraigo acoples
+f=open(acople,'r')
+lines=f.readlines()[1:]
+
+key=[]
+archivo=[]
+
+for line in lines:
+    p=line.split()
+    key.append("%0.2f" % float(p[0]))
+    archivo.append("E-T"+"-""%d" % float(n)+"-"+"%0.2f" % float(p[0])+".txt")
+
+        
+
+#extraigo de los archivos E-T
+for i in range(0,len(key)):
+ f=open(archivo[i],'r')
+ lines=f.readlines()[1:]
+ 
+ t=[]
+ ce=[]
+ #deprom=[]
+ 
+ for line in lines: 
+      p = line.split()
+      t.append(float(p[0]))
+      ce.append(float(p[2])*float(p[2])/(float(p[0])*float(p[0])))
+      
+ tv=np.array(t)
+ cev=np.array(ce)
+ calor[i]=[tv,cev] 
+ f.close()
+
+
+#colores
+color=['bo','ro','yo','mo','co','go','ko','ro']
+colorline=['b','r','y','m','c','g','k','r']
+
+
+#Grafico EvsT
+for i in range(0,len(key)):
+ #plt.errorbar(energia[i][0], energia[i][1], energia[i][2],color=colorline[i])
+ plt.plot(calor[i][0],calor[i][1],colorline[i])
+ plt.plot(calor[i][0],calor[i][1],color[i],label=key[i]) 
+plt.xlabel("Temperatura")
+plt.ylabel("calor especifico")
+plt.legend(loc='upper right')
+plt.show()
+
+
